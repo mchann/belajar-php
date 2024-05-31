e2eb73e83e07182228916f17d0c8406da7895529
mchann <echaazwaputrialvianti@gmail.com>
2024-05-31 10:02:27 +0700

    kondisi dan perbandingan

---
+++ 2-variable.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama = "Rezha ajwa";
+$umur = 19 ;
+$beratBadan = 53;
+$sudahMenikah = true;
+
+echo "Nama: " . $nama . "<br>";
+echo "Umur: " . $umur . " tahun <br>";
+echo "Berat Badan : " . $beratBadan . "kg<br>";
+
+
+?>
---
+++ 3-kondisi-dan-perbandingan.php
@@ -0,0 +1,46 @@
+<?php
+
+$a = 10 ;
+$b = 20;
+
+// operator ==
+if ($a == $b){
+    echo "\$a sama dengan \$b<br>";
+} else {
+    echo "\$a tidak sama dengan \$b<br>";
+}
+
+// operator!=
+
+if ($a != $b){
+    echo "\$a lebih besar dari \$b<br>";
+}
+
+// oprator >
+
+if ($a > $b){
+    echo "\$a lebih besar dari \$b<br>";
+} else {
+    echo "\$a tidak lebih besar dari \$<br>";
+}
+
+// Operator <
+
+if ($a < $b){
+    echo "\$a lebih kecil dari \$b<br>";
+}
+
+// Operator >=
+
+if ($a >= $b){
+    echo "\$a lebih besar atau sama dengan \$b<br>";
+} else {
+    echo "\$a tidak lebih besar atau sama dengan \$b<br>";
+}
+
+if ($a <= $b){
+    echo "\$a lebih kecil atau sama dengan \$b<br>";
+}
+
+
+?>
